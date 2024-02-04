87a90fbbd972e20747714dcdddba05c9dfd7ae2c
TaraskiN85 <taras12304@i.ua>
2024-02-04 11:12:31 +0200

    initialize project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+import { PhonebookForm } from "./Form.jsx/PhonebookForm";
+import {Contacts} from "./Contacts/Contacts";
+import { Filter } from "./Filter/Filter";
+
+import css from './App.module.css';
+
 export const App = () => {
+
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('phonebook')) || [])
+  const [filter, setFilter] = useState('')
+  
+  useEffect(() => { 
+      localStorage.setItem("phonebook", JSON.stringify(contacts))
+  }, [contacts])
+
+  const addUser = (formData => {
+    const isDuplicate = contacts.some(contact => contact.name === formData.name)
+    
+    if (isDuplicate) {
+      alert(`${formData.name} is already in contacts.`)
+      return
+    }
+
+    setContacts(prevContacts => [...prevContacts, formData])
+  })
+
+  const handleSearch = (searchData => setFilter(searchData))
+
+  const handleDelete = (name) => {
+    setContacts(contacts.filter(contact => contact.name !== name))
+  }
+
+  const filteredContacts = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase())
+  })
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.phonebook}>
+      <h1 className={css.phonebookTitle}>Phonebook</h1>
+      <PhonebookForm addUser={addUser} />
+      <h2 className={css.contactsTitle}>Contacts</h2>
+      <Filter
+        value={filter}
+        dataSearch={handleSearch}
+      />
+      <Contacts contacts={filteredContacts} deleteContact={handleDelete} />
     </div>
-  );
-};
+  )
+}
